a8be5348a63543808f7726995d2d4a5b4f986b28
GuyLyu <guy.lyuboshits@gmail.com>
2021-10-24 11:47:38 +0300

    Changed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,9 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bjjnique
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
